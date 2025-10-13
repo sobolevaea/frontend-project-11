5124652c954f4d9ff62a05ca5c6b8c0e3dde66e8
sobolevaea <liziaka@mail.ru>
2025-10-13 19:16:16 +0300

    feat: edit ru.js

--- locale/ru.js
+++ locale/ru.js
@@ -4,17 +4,17 @@ export default {
       errors: {
         notUrl: 'Ссылка должна быть валидным URL',
         exists: 'RSS уже существует',
+        notRss: 'Ресурс не содержит валидный RSS',
+        badConnection: 'Ошибка сети',
       },
       success: 'RSS успешно загружен',
     },
     cards: {
       feeds: {
         cardTitle: 'Фиды',
-        // subtitle и text ТОЧНО нужно переработать позже
-        subtitle: 'Lorem ipsum feed for an interval of 1 minutes with 10 item(s)',
-        text: 'This is a constantly updating lorem ipsum feed',
       },
       posts: {
+        cardTitle: 'Посты',
         viewButton: 'Просмотр',
       },
     },
